2d556e07ab409eef9b7ae638cad134932a7bf451
didilinkin <929213769@qq.com>
2018-12-07 22:51:52 +0800

    :package: Add redux-saga

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,38 @@
 import React from 'react'
-import Link from 'next/link'
+// import Link from 'next/link'
+import { connect } from 'react-redux'
 
-export default () => (
-  <ul>
-    <li><Link href='/a' as='/a'><a>a</a></Link></li>
-    <li><Link href='/b' as='/b'><a>b</a></Link></li>
-  </ul>
-)
+import { loadData, startClock, tickClock } from '../saga/actions'
+import Page from '../components/page'
+
+// export default () => (
+//   <ul>
+//     <li><Link href='/a' as='/a'><a>a</a></Link></li>
+//     <li><Link href='/b' as='/b'><a>b</a></Link></li>
+//   </ul>
+// )
+
+interface Props {
+  dispatch: Function,
+}
+
+class Index extends React.Component<Props, {}> {
+  static async getInitialProps(props) {
+    const { store, isServer } = props.ctx
+    store.dispatch(tickClock(isServer))
+    if (!store.getState().placeholderData) {
+      store.dispatch(loadData())
+    }
+    return { isServer }
+  }
+
+  componentDidMount() {
+    this.props.dispatch(startClock())
+  }
+
+  render() {
+    return <Page title='Index Page' linkTo='/other' NavigateTo='Other Page' />
+  }
+}
+
+export default connect()(Index)
---
+++ pages/other.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {connect} from 'react-redux'
+
+import {startClock, tickClock} from '../saga/actions'
+import Page from '../components/page'
+
+interface Props {
+  dispatch: Function,
+}
+
+class Other extends React.Component<Props, {}> {
+  static async getInitialProps (props) {
+    const { store, isServer } = props.ctx
+    store.dispatch(tickClock(isServer))
+    return { isServer }
+  }
+
+  componentDidMount () {
+    this.props.dispatch(startClock())
+  }
+
+  render () {
+    return <Page title='Other Page' linkTo='/' NavigateTo='Index Page' />
+  }
+}
+
+export default connect()(Other)
