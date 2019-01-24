0bc4f5297df16d3ab0deb3789a76e31195068168
didilinkin <929213769@qq.com>
2019-01-24 17:46:07 +0800

    :white_check_mark: Add Jest => success

--- components/Clock.tsx
+++ components/Clock.tsx
@@ -17,16 +17,4 @@ interface Props {
 export default ({ lastUpdate, light }: Props) =>
   <div className={light ? 'light' : ''}>
     {format(new Date(lastUpdate))}
-    {/* <style jsx>{`
-        div {
-          padding: 15px;
-          display: inline-block;
-          color: #82FA58;
-          font: 50px menlo, monaco, monospace;
-          background-color: #000;
-        }
-        .light {
-          background-color: #999;
-        }
-      `}</style> */}
   </div>
--- utils/sum/__tests__/sum.test.ts
+++ utils/sum/__tests__/sum.test.ts
@@ -2,16 +2,11 @@
  * @Author: yanxiaodi 929213769@qq.com
  * @Date: 2019-01-23 18:08:08
  * @LastEditors: yanxiaodi 929213769@qq.com
- * @LastEditTime: 2019-01-23 18:10:31
+ * @LastEditTime: 2019-01-24 17:42:13
  * @Description:
  */
 import sum from '../index'
 
-describe("测试", () => {
-  test('Add', () => {
-    expect(
-      sum(1, 2)
-    )
-      .toBe(3)
-  })
+test('测试 func: sum', () => {
+  expect(sum(1, 2)).toBe(3)
 })
